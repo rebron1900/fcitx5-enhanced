c40b9a3b34c747515d5168c32cdda0c18e0be8bf
rebron1900 <rebron1900@users.noreply.github.com>
2026-06-12 00:48:22 +0800

    chore: bump v1.3.1 → v1.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rebron1900.fcitx5enhanced"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.1"
+        versionCode = 11
+        versionName = "1.4.0"
     }
 
     buildTypes {
