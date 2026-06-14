127496858c3f8483f9646381b2245ea37248159e
H小姐 <h@hermes.local>
2026-06-14 18:05:26 +0800

    bump version to 1.5.2 (code=14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.rebron1900.fcitx5enhanced"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.5.0"
+        versionCode = 14
+        versionName = "1.5.2"
     }
 
     buildTypes {
